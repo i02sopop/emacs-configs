18ddddce208dc4471e73b078fa626aeef7d61c19
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2018-09-14 18:42:42 -0500

    Add org2jekyll configurations.

--- org-mode.el
+++ org-mode.el
@@ -40,8 +40,64 @@
 (use-package org-fstree
   :ensure t)
 
-(use-package org-jekyll
-  :ensure t)
+(use-package org2jekyll
+  :ensure t
+  :config
+  (setq org2jekyll-blog-author "Ritho")
+  (setq org2jekyll-source-directory (expand-file-name "~/org/blog/"))
+  (setq org2jekyll-jekyll-directory (expand-file-name "~/desarrollo/ritho/github/i02sopop/blog/"))
+
+  (setq org2jekyll-jekyll-drafts-dir "")
+  (setq org2jekyll-jekyll-posts-dir "_posts/")
+  (setq org-publish-project-alist
+		`(("default"
+		  :base-directory ,(org2jekyll-input-directory)
+		  :base-extension "org"
+		  :publishing-directory ,(org2jekyll-output-directory)
+		  :publishing-function org-html-publish-to-html
+		  :headline-levels 4
+		  :section-numbers nil
+		  :with-toc nil
+		  :html-head "<link rel=\"stylesheet\" href=\"./css/style.css\" type=\"text/css\"/>"
+		  :html-preamble t
+		  :recursive t
+		  :make-index t
+		  :html-extension "html"
+		  :body-only t)
+		 ("post"
+		  :base-directory ,(org2jekyll-input-directory)
+		  :base-extension "org"
+		  :publishing-directory ,(org2jekyll-output-directory org2jekyll-jekyll-posts-dir)
+		  :publishing-function org-html-publish-to-html
+		  :headline-levels 4
+		  :section-numbers nil
+		  :with-toc nil
+		  :html-head "<link rel=\"stylesheet\" href=\"./css/style.css\" type=\"text/css\"/>"
+		  :html-preamble t
+		  :recursive t
+		  :make-index t
+		  :html-extension "html"
+		  :body-only t)
+		 ("images"
+		  :base-directory ,(org2jekyll-input-directory "img")
+		  :base-extension "jpg\\|gif\\|png"
+		  :publishing-directory ,(org2jekyll-output-directory "img")
+		  :publishing-function org-publish-attachment
+		  :recursive t)
+		 ("js"
+		  :base-directory ,(org2jekyll-input-directory "js")
+		  :base-extension "js"
+		  :publishing-directory ,(org2jekyll-output-directory "js")
+		  :publishing-function org-publish-attachment
+		  :recursive t)
+		 ("css"
+		  :base-directory ,(org2jekyll-input-directory "css")
+		  :base-extension "css\\|el"
+		  :publishing-directory ,(org2jekyll-output-directory "css")
+		  :publishing-function org-publish-attachment
+		  :recursive t)
+		 ("web" :components ("images" "js" "css"))))
+  )
 
 (use-package org-jira
   :ensure t)
