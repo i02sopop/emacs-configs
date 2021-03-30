3040e986e3429715738382ba8e386158579d976a
Ritho <palvarez@ritho.net>
2021-03-30 23:13:40 +0200

    Improve emacs configurations
    * Add magit to the configurations.
    * Add php to the configurations
    * Improve golangci

---
+++ php.el
@@ -0,0 +1,33 @@
+(use-package php-mode
+  :ensure t)
+
+(use-package php-refactor-mode
+  :ensure t)
+
+(use-package phps-mode
+  :ensure t)
+
+(use-package phpstan
+  :ensure t)
+
+(use-package ac-php
+  :ensure t)
+
+(use-package ac-php-core
+  :ensure t)
+
+(use-package flymake-php
+  :ensure t)
+
+(use-package flymake-phpcs
+  :ensure t)
+
+(use-package phpunit
+  :ensure t)
+
+;; (use-package lsp-mode
+;;   :ensure t
+;;   :commands (lsp lsp-deferred)
+;;   :hook (go-mode . lsp-deferred)
+;;   :config
+;; )
