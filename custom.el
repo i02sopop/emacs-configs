7196e84ef581345e54627216d9ec58633ab780a4
Ritho <palvarez@ritho.net>
2022-10-04 21:13:44 +0200

    Change custom file.
    Signed-off-by: Ritho <palvarez@ritho.net>

--- custom.el
+++ custom.el
@@ -3,15 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(canlock-password "833f6b2804c517c116d2545efb09139814a018b7")
  '(custom-safe-themes
    '("1f0324928d94805ffb9b773ed4af86301d1829f878fc96402e23bdaf11d95bb5" default))
  '(ediff-diff-options "-w")
  '(ediff-split-window-function 'split-window-horizontally)
  '(ediff-window-setup-function 'ediff-setup-windows-plain)
  '(org-trello-current-prefix-keybinding "C-c o" nil (org-trello))
- '(package-selected-packages
-   '(mix sql-indent vue-html-mode vue-mode go-expr-completion mu4e-maildirs-extension org-clubhouse magit-org-todos magit phpunit flymake-phpcs flymake-php ac-php phpstan phps-mode php-refactor-mode php-mode emacs-droid javarun java-file-create eldoro which-key wc-mode use-package scala-mode salt-mode pocket-reader outorg org2jekyll org2issue org-wc org-trello org-table-sticky-header org-table-comment org-sync org-super-agenda org-review org-projectile org-present org-pomodoro org-notebook org-mobile-sync org-mime org-make-toc org-link-travis org-journal org-jira org-elisp-help org-ehtml org-dashboard org-context org-clock-today org-caldav org-bullets org-alert org-agenda-property org-ac oauth2 meghanada markdownfmt markdown-toc markdown-mode+ lsp-ui latex-extra jtags js2-refactor javap-mode javadoc-lookup java-snippets java-imports gotest golint go-tag go-stacktracer go-snippets go-projectile go-imports go-impl go-imenu go-gopath go-gen-test go-fill-struct go-errcheck go-direx go-complete go-capf go-autocomplete go-add-tags go gh-md flyspell-correct-popup flymd flex-autopair epresent calfw-org calfw-gcal calfw-cal calfw bpe bibtex-utils bibslurp bibretrieve bbdb2erc bbdb-ext auto-dictionary auto-complete-auctex auctex-latexmk android-mode ace-flyspell ac-math ac-js2 ac-html-csswatcher ac-html-bootstrap ac-html ac-etags ac-capf ac-c-headers)))
+ '(package-selected-packages '(use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
