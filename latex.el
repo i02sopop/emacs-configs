47a1bc86176580b1c773a6cd77f56ee9cd97aa60
Ritho <palvarez@ritho.net>
2022-10-10 23:26:13 +0200

    Add a couple of latex clases.
    Signed-off-by: Ritho <palvarez@ritho.net>

--- latex.el
+++ latex.el
@@ -4,6 +4,26 @@
   (setq TeX-auto-save t)
   (setq TeX-parse-self t)
   (setq-default TeX-master nil)
+
+  (add-to-list 'org-latex-classes
+			   '("report-noparts"
+				 "\\documentclass{report}"
+				 ("\\chapter{%s}" . "\\chapter*{%s}")
+				 ("\\section{%s}" . "\\section*{%s}")
+				 ("\\subsection{%s}" . "\\subsection*{%s}")
+				 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+				 ("\\paragraph{%s}" . "\\paragraph*{%s}")
+				 ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
+  (add-to-list 'org-latex-classes
+			   '("book-noparts"
+				 "\\documentclass{book}"
+				 ("\\chapter{%s}" . "\\chapter*{%s}")
+				 ("\\section{%s}" . "\\section*{%s}")
+				 ("\\subsection{%s}" . "\\subsection*{%s}")
+				 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+				 ("\\paragraph{%s}" . "\\paragraph*{%s}")
+				 ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
   )
 
 (use-package auctex-latexmk
