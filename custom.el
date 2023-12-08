45670289b1a0213fbedaefec11e108efa4d8ab53
Ritho <palvarez@ritho.net>
2023-12-08 19:42:00 +0100

    Update the selected packages.
    Signed-off-by: Ritho <palvarez@ritho.net>

--- custom.el
+++ custom.el
@@ -4,13 +4,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("1f0324928d94805ffb9b773ed4af86301d1829f878fc96402e23bdaf11d95bb5" default))
+   '("1f0324928d94805ffb9b773ed4af86301d1829f878fc96402e23bdaf11d95bb5"
+	 default))
  '(ediff-diff-options "-w")
  '(ediff-split-window-function 'split-window-horizontally)
  '(ediff-window-setup-function 'ediff-setup-windows-plain)
  '(org-trello-current-prefix-keybinding "C-c o")
- '(package-selected-packages
-   '(gitlab-ci-mode gitlab-ci-mode-flycheck gitlab-pipeline magit-org-todos magit ac-php ac-php-core 0blayout php-scratch php-refactor-mode podcaster pocket-mode gh-md latex-extra bibtex-utils bibslurp bibretrieve lsp-mode lsp-ui go-expr-completion go-imenu go-impl go-snippets go-stacktracer go-tag go-rename go-eldoc go-errcheck go-guru go-autocomplete go-gopath org-super-agenda pocket-reader gptel tide typescript-mode react-snippets helm-core magit-section with-editor tree-sitter js3-mode org-analyzer org-books org-dashboard org-gtd org-kanban org-notebook org-notify org-plus-contrib godoctor use-package)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
