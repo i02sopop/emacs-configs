24ff571ae0df7546a2d83bdf9f346c38bd330210
Ritho <palvarez@ritho.net>
2026-02-02 00:08:47 +0100

    Update the copilot configs.
    Signed-off-by: Ritho <palvarez@ritho.net>

--- copilot-configs.el
+++ copilot-configs.el
@@ -29,4 +29,43 @@
   (add-to-list 'copilot-indentation-alist '(rst-mode 4))
   (add-to-list 'copilot-indentation-alist '(text-mode 2))
   (add-to-list 'copilot-indentation-alist '(ts-mode 4))
+
+  (defun rk/no-copilot-mode ()
+	"Helper for `rk/no-copilot-modes'."
+	(copilot-mode -1))
+
+  (defvar rk/no-copilot-modes '(debugger-mode
+								fundamental-mode)
+	"List of modes where `copilot-mode' should be disabled.")
+
+  (defun rk/copilot-disable-predicate ()
+	"When copilot should not automatically show completions."
+	(or rk/copilot-manual-mode
+		(member major-mode rk/no-copilot-modes)
+		(company--active-p)))
+
+  (add-to-list 'copilot-disable-predicates #'rk/copilot-disable-predicate)
+
+  (defvar rk/copilot-manual-mode nil
+	"When `t' will only show completions when manually triggered, e.g. via M-C-<return>.")
+
+  (defun rk/copilot-change-activation ()
+	"Switch between three activation modes:
+- automatic: copilot will automatically overlay completions
+- manual: you need to press a key (M-C-<return>) to trigger completions
+- off: copilot is completely disabled."
+	(interactive)
+	(if (and copilot-mode rk/copilot-manual-mode)
+		(progn
+          (message "deactivating copilot")
+          (global-copilot-mode -1)
+          (setq rk/copilot-manual-mode nil))
+      (if copilot-mode
+          (progn
+			(message "activating copilot manual mode")
+			(setq rk/copilot-manual-mode t))
+		(message "activating copilot mode")
+		(global-copilot-mode))))
+
+  (define-key global-map (kbd "M-C-<escape>") #'rk/copilot-change-activation)
   )
